94ab228dbfa04c60657f3f63b983627c73f8f9e0
Aaron Roach <37710170+atroach@users.noreply.github.com>
2018-06-28 13:02:30 -0400

    Modularity update
    Changed explicit variables to arrays, so that new executeables can be added and removed easier than before

--- OfficeShortcuts.ps1
+++ OfficeShortcuts.ps1
@@ -1,18 +1,22 @@
 #OfficeShortcuts -- Creates shortcuts for the main four Office 2016 applications.
-$ExeName = "WINWORD.exe"
-$ExeAlias = "Word"
 
+[string[]] $Office16ExeNames = @("WINWORD.EXE", "EXCEL.EXE", "POWERPNT.EXE", "OUTLOOK.EXE")
+[string[]] $Office16ExeAliases = @("Word", "Excel", "PowerPoint", "Outlook")
 
 #Creates a WScript.Shell object, sets the target path and destination, and saves the shortcut with a more readable name.
 #Accepts the .exe file name and readable shortcut name as arguments.
-function CreateOfficeShortcut([string]$MyName, [string]$MyAlias)
+function CreateOfficeShortcut([string]$ExeName, [string]$ExeAlias)
 {
-    $TargetFile = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\$MyName"
-    $ShortcutFile = "$env:USERPROFILE\Desktop\$MyAlias 2016.lnk"
+    $TargetFile = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\$ExeName"
+    $ShortcutFile = "$env:USERPROFILE\Desktop\$ExeAlias 2016.lnk"
     $WScriptShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
     $Shortcut.TargetPath = $TargetFile
     $Shortcut.Save()
 }
 
-CreateOfficeShortcut $ExeName $ExeAlias
+#Loop through the array of executable names, and create a shortcut with a matching alias
+for ($i=0; $i -lt $Office16ExeNames.Length; $i++)
+{
+    CreateOfficeShortcut $Office16ExeNames[$i] $Office16ExeAliases[$i]
+}
