03178402d611fa0db24d0ff14a8fb4288d9afa42
Aaron Roach <37710170+atroach@users.noreply.github.com>
2018-06-29 00:34:54 -0400

    Final commit for the day
    Almost fully working, but the -c for "custom" switch for the script is not working. The script fully terminates when exiting the do/while loop in function "GetCustomUserSelection"

--- OfficeShortcuts.ps1
+++ OfficeShortcuts.ps1
@@ -1,22 +1,88 @@
 #OfficeShortcuts -- Creates shortcuts for the main four Office 2016 applications.
 
-[string[]] $Office16ExeNames = @("WINWORD.EXE", "EXCEL.EXE", "POWERPNT.EXE", "OUTLOOK.EXE")
-[string[]] $Office16ExeAliases = @("Word", "Excel", "PowerPoint", "Outlook")
+#Optional command-line parameters that can be passed to create icons for All (-a) icons, or a CUSTOM set of icons (-c)
+param([switch] $a, [switch] $c)
 
-#Creates a WScript.Shell object, sets the target path and destination, and saves the shortcut with a more readable name.
-#Accepts the .exe file name and readable shortcut name as arguments.
-function CreateOfficeShortcut([string]$ExeName, [string]$ExeAlias)
+#Creates a shortcut on the desktop that copies from the start menu shortcuts.
+function CreateOfficeDesktopShortcut([string] $ShortcutName)
 {
-    $TargetFile = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\$ExeName"
-    $ShortcutFile = "$env:USERPROFILE\Desktop\$ExeAlias 2016.lnk"
-    $WScriptShell = New-Object -ComObject WScript.Shell
-    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-    $Shortcut.TargetPath = $TargetFile
-    $Shortcut.Save()
+    Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" "$env:USERPROFILE\Desktop"
 }
 
-#Loop through the array of executable names, and create a shortcut with a matching alias
-for ($i=0; $i -lt $Office16ExeNames.Length; $i++)
+function Show-Menu
 {
-    CreateOfficeShortcut $Office16ExeNames[$i] $Office16ExeAliases[$i]
+     Clear-Host
+     Write-Host "Select which items you would like placed on the desktop:"
+     
+     Write-Host "1: Word"
+     Write-Host "2: Excel"
+     Write-Host "3: PowerPoint"
+     Write-Host "4: Outlook"
+     Write-Host "5: Access"
+     Write-Host "6: OneNote"
+     Write-Host "7: Publisher"
+     Write-Host "D: Press 'D' when done."
 }
+
+function GetCustomUserSelection()
+{
+    [string[]]$CustomSelection = @()
+    do
+    {
+        Clear-Host
+        Show-Menu
+        Write-Host "You have selected: $CustomSelection"
+        $input = Read-Host "Please make a selection:"
+        switch($input)
+        {
+            '1'{
+                $CustomSelection += "Word"
+            }
+            '2'{
+                $CustomSelection += "Excel"
+            }
+            '3'{
+                $CustomSelection += "PowerPoint"
+            }
+            '4'{
+                $CustomSelection += "Outlook"
+            }
+            '5'{
+                $CustomSelection += "Access"
+            }
+            '6'{
+                $CustomSelection += "OneNote"
+            }
+            '7'{
+                CustomSelection += "Publisher"
+            }
+            'd'{
+                return
+            }
+        }
+    }
+    until ($input -eq 'd')
+    "Now do something else"
+}
+
+#Loop through the array of Shortcut names, and create a shortcut with a matching alias
+function SelectOfficeShortcuts($arg1, $arg2)
+{
+    [string[]] $OfficeProgramNames = @("Word", "Excel", "PowerPoint", "Outlook")
+
+    if ($a -eq $True)
+    {
+        $OfficeProgramNames += "Access", "OneNote*", "Publisher"
+    }
+    elseif($c -eq $True)
+    {
+        $OfficeProgramNames = @()
+        GetCustomUserSelection $OfficeProgramNames
+    }
+    for ($i=0; $i -lt $OfficeProgramNames.Length; $i++)
+    {
+        CreateOfficeDesktopShortcut $OfficeProgramNames[$i]
+    }
+}
+
+SelectOfficeShortcuts($a, $c)
